fa5871a21a6ec007687235f49ba5982559bcc572
Hector Sanchez <f1.hectorsan@gmail.com>
2024-02-15 17:43:57 -0800

    logo and homecomponent works

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by kind">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+
+    </section>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
