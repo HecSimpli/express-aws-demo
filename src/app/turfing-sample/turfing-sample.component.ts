9c679b436d83c9f56612fc1ba70787665b54d611
Hector Sanchez <f1.hectorsan@gmail.com>
2024-02-15 20:45:02 -0800

    linking turf-sample.ts with tufing-sample.component.ts

--- src/app/turfing-sample/turfing-sample.component.ts
+++ src/app/turfing-sample/turfing-sample.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TurfSample } from '../turf-sample';
 
 @Component({
   selector: 'app-turfing-sample',
@@ -7,13 +8,14 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <section class="listing">
-      <img class="listing-photo">
-      <h2 class="listing-heading"></h2>
-      <p class="listing-sample"></p>
+      <img class="listing-photo" [src]="turfingSample.
+      photo" alt="photo of {{turfingSample.name}}">
+      <h2 class="listing-heading">{{turfingSample.name}}</h2>
+      <p class="listing-sample">{{turfingSample.oz}}, {{turfingSample.pileHeight}}</p>
     </section>
   `,
   styleUrl: './turfing-sample.component.css'
 })
 export class TurfingSampleComponent {
-
+  @Input() turfingSample!:TurfSample;
 }
